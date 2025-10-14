ff1bf879bea8591ebb77aaacbc68834d593a539b
Bagus-kunc <bagus.kuncoro@talenavi.com>
2025-10-14 11:43:38 +0700

    fix: [BK] enter redeem

--- pages/redeem/[id].vue
+++ pages/redeem/[id].vue
@@ -722,7 +722,7 @@ const validateInput = (field, value) => {
   }
 }
 
-const handleApiError = (error) => {
+const handleApiError = async (error) => {
   errorScroll.value = []
 
   const response = error._data?.message || {}
@@ -736,7 +736,10 @@ const handleApiError = (error) => {
 
     errorScroll.value = message
     errorMessages.value.push(response)
-    insufficientDialogVisible.value = true
+    if (errorScroll.value.length === 0) {
+      await nextTick()
+      insufficientDialogVisible.value = true
+    }
   }
 }
 
@@ -871,10 +874,9 @@ const checkPostalCode = async (code) => {
   }
 }
 
-
 const handleKeydown = (event) => {
   if (event.key === 'Enter') {
-    if (!isLoading.value && !hasModal.value && !disableRedeem.value) {
+    if (!isLoading.value && !insufficientDialogVisible.value && !disableRedeem.value) {
       handleSubmit()
     }
   }
