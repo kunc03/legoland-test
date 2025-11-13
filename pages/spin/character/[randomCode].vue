7ae47802e71f6ef1e5a6d4f6d1f853d1bf57b632
Alga <vaniaalga@gmail.com>
2025-11-13 11:15:10 +0700

    fix: [AV] remove console log slug data

--- pages/spin/character/[randomCode].vue
+++ pages/spin/character/[randomCode].vue
@@ -354,7 +354,6 @@ const fetchImage = async () => {
     const slug = parsedData.slug.toUpperCase()
 
     const slugData = decryptData(localStorage.getItem(`${slug}_GACHA`))
-    console.log('Slug Data:', slugData)
     characterImageUrl.value = slugData?.character_image
     charName.value = slugData?.character_name
     raritySrc.value = slugData?.character_rarity
