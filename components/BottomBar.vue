e4722d0ae39d4e944b8228a2f345a55f6185764a
rifqi <rifqi.ainun@talenavi.com>
2025-12-12 09:24:11 +0700

    fix: [RA] dashboard point count

--- components/BottomBar.vue
+++ components/BottomBar.vue
@@ -37,7 +37,7 @@
                 settings.user_dashboard?.footers?.point_background_color?.color,
             }"
           >
-            {{ store.footer_point }}<span class="text-exd-1020">{{ settings.user_dashboard?.footers?.point_suffix }}</span>
+            {{ store.footer_point }}<span class="text-exd-1020">{{ settings.user_dashboard?.footers?.point_suffix != undefined ? settings.user_dashboard?.footers?.point_suffix : 'pt' }}</span>
           </p>
         </div>
       </div>
