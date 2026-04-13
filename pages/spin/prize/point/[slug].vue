f397cc3868eb6789facecc7564894781758ea53e
Bagus Kunc <bagus.kuncoro@talenavi.com>
2026-04-13 20:04:57 +0700

    fix: [BK] mechine gacha in prize spin point

--- pages/spin/prize/point/[slug].vue
+++ pages/spin/prize/point/[slug].vue
@@ -13,9 +13,10 @@
     <SparkleStart className="top-3 z-30" />
 
     <img
-      :src="settings?.global?.gacha_machine_image"
+      :src="externalRedeemStore.isExternalRedeem ? gachaTom : settings?.global?.gacha_machine_image"
       alt="gacha2"
       class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[120%] max-w-none h-auto max-h-[96svh] object-contain"
+      :class="externalRedeemStore.isExternalRedeem ? 'w-[80%] max-w-none' : 'w-[120%] max-w-none'"
       loading="eager"
       decoding="async"
       fetchpriority="high"
