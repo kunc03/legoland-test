138ffd1bd06832d351e1663b508624a1d34f6fb8
Bagus-kunc <bagus.kuncoro@talenavi.com>
2026-04-13 13:56:22 +0700

    fix: [BK] header height

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="pt-[39px] pb-3 flex justify-between items-center top-0 left-0 right-0 z-40 fixed w-full max-w-md mx-auto "
+    class="h-[102px] flex justify-between items-center top-0 left-0 right-0 z-40 fixed w-full max-w-md mx-auto "
     :style="{ 'box-shadow': '0px 3px 3px 0px rgba(0, 0, 0, 0.1608)', 'background': settings.global?.global_header_footer_color }"
     @touchmove="(e) => e.preventDefault()"
   >
