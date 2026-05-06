1bee8c85102ec1b626a1ad0b558305720ac52afc
Bagus-kunc <bagus.kuncoro@talenavi.com>
2026-05-06 14:04:31 +0700

    fix: [BK] bottom copyright

--- components/SplashScreen.vue
+++ components/SplashScreen.vue
@@ -219,6 +219,6 @@ onUnmounted(() => {
         class="mt-6 ml-5 w-[126px] h-[24px]"
       /> -->
     </div>
-    <Copyright class="absolute left-0 right-0 bottom-[calc(5.05rem+env(safe-area-inset-bottom))]" />
+    <Copyright class="absolute bottom-0 left-0 right-0" />
   </div>
 </template>
